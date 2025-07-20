815f3ad3f156d31eaf8e8ed98dbc0b4ba1ba3938
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-07-20 02:38:27 -0300

    analisar aula 103 pra correcao

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -33,15 +33,96 @@ class MainController extends Controller
                'check_subtraction'=>'required_without_all::check_multiplication,check_division,check_sum',
                'check_division'=>'required_without_all::check_multiplication,check_sum,check_subtraction',
                'check_multiplication'=>'required_without_all::check_sum,check_subtraction,check_division',
-               'number_one'=>'required|integer|min:0|max:999',
+               'number_one'=>'required|integer|min:0|max:999|lt:number_two', // a regra lt diz que o numero 1 deve ser menor que o numero 2
                'number_two'=>'required|integer|min:0|max:999',
                'number_exercises'=>'required|integer|min:5|max:50'
 
           ]
      );
-     //serve pra verificar oque veio na requisição 
-     dd($request->all());
 
+     //pegando as operações selecionadas 
+     //no caso eu vou criar um array que vai receber a operação dependedo doque vier na requisição
+     //testando aqui eu uso um operador ternario pra testar cada um dos imputs 
+     // se o check_sum vier como checado(selecionado) o primiero indice do array recebe a operação sum 
+     //e assim por diante
+
+     $operations = [];
+     $operations[] = $request->check_sum ? 'sum' : '';
+     $operations[] = $request->check_subtraction ? 'subtraction' : '';
+     $operations[] = $request->check_multiplication ? 'multiplication' : '';
+     $operations[] = $request->check_division ? 'division' : '';
+
+     //pegando os numeros minimos e maximos 
+
+     $min  = $request->number_one;
+     $max  = $request->number_two;
+     
+     //pegando a quantidade de execicios 
+
+     $numberExercises = $request->number_exercises;
+
+     //gerando os exercicios 
+
+     $exercises = [];
+
+     for($index = 1; $index <= $numberExercises; $index ++){
+
+          //a aperação vau receber um indice randomico de dentro array de operações 
+
+          $operation = $operations[array_rand($operations)];
+          $number1 = rand($min,$max);
+          $number2 = rand($min,$max);
+
+          $exercise = '';
+          $sollution = '';
+
+          switch($operation){
+
+               case 'sum':
+                     $exercise = "$number1 + $number2";
+                     $sollution = $number1 + $number2;
+               break;
+
+
+               case 'subtraction':
+                     $exercise = "$number1 - $number2";
+                     $sollution = $number1 - $number2;
+               break;
+
+               case 'division':
+                     $exercise = "$number1 : $number2";
+                     $sollution = $number1 / $number2;
+               break;
+
+               case 'multiplication':
+                     $exercise = "$number1 * $number2";
+                     $sollution = $number1 * $number2;
+               break;
+
+                 
+          }
+
+          $exercises[]=[
+
+               'exercises' => $index,
+               'exercise' => $exercise,
+               'sollution' => "$exercise $sollution"
+          ];
+
+          dd($exercises);
+             
+
+
+
+
+     }
+
+
+
+
+
+
+     
        
     }
 
