159ed19f73820c1859e7a854ed388c3776f2823d
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-07-20 00:52:02 -0300

    controllers adicionados e view também

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function home(){
-         echo 'Apresentar a pagina inicial';
+    public function home():View
+    
+    {
+         return view('home');
     }
 
+
+    
     public function gerarExercicios(){
 
          echo 'Gerar Exercios';
